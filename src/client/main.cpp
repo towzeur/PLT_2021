5998fd8fe09ad2db0ef4fd412e61767d8d7f8cad
towzeur <towzeur@gmail.com>
2020-10-10 13:11:31 +0200

    added Livrable 1.1

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -9,17 +9,33 @@ void testSFML() {
 
 // Fin test SFML
 
-#include <state.h>
+//#include <state.h>
+#include <string.h>
 
 using namespace std;
-using namespace state;
+//using namespace state;
 
-int main(int argc,char* argv[])
+const char* HELP_MESSAGE =R"V0G0N(
+usage: client <command> [<args>]
+
+    hello       Print a Hello World message to the console.
+)V0G0N";
+
+int main(int argc, char* argv[])
 {
-    Exemple exemple;
-    exemple.setX(53);
+    //Exemple exemple;
+    //exemple.setX(53);
 
-    cout << "It works !" << endl;
+
+    // parse arguments
+    if (argc>1) {
+        if (strncmp(argv[1], "hello", 5) == 0) {
+            cout << "Hello PLT !" << endl;
+        }
+    }
+    else {
+        cout << HELP_MESSAGE << endl;
+    }
 
     return 0;
 }
