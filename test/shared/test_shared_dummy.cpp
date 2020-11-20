c03a137e3fe15dd12f53df39d12aa303270bf46d
Badisse <badisse.bouabdallahb@laposte.net>
2020-11-20 17:48:11 +0100

    Update test. Fix jenkins bug

--- test/shared/test_shared_dummy.cpp
+++ test/shared/test_shared_dummy.cpp
@@ -64,7 +64,7 @@ BOOST_AUTO_TEST_CASE(TestExemple) {
     Territory tr;
 
     // getUid
-    // BOOST_CHECK(tr.getUid());
+    BOOST_CHECK_EQUAL(tr.getUid(), 0);
 
     // CapitalRow getter and setter
     tr.setCapitalRow(5);
@@ -105,7 +105,7 @@ BOOST_AUTO_TEST_CASE(TestExemple) {
     Player pl;
 
     // getUid
-    //  BOOST_CHECK(pl.getUid());
+    BOOST_CHECK_EQUAL(pl.getUid(), 0);
 
     // Name getter and setter
     std::string name = "Luffy";
