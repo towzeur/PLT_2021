b5a736470855be1471342246f3f81dddf9864f17
Badisse <badisse.bouabdallahb@laposte.net>
2020-11-20 23:02:28 +0100

    Update test for render

--- test/client/test_client_dummy.cpp
+++ test/client/test_client_dummy.cpp
@@ -1,17 +1,29 @@
-#include <boost/test/unit_test.hpp>
+#include "../../src/client/render.h"
 #include <SFML/Graphics.hpp>
+#include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(TestStaticAssert)
-{
-  BOOST_CHECK(1);
+using namespace render;
+
+BOOST_AUTO_TEST_CASE(TestStaticAssert) { BOOST_CHECK(1); }
+
+BOOST_AUTO_TEST_CASE(TestSFML) {
+  {
+    ::sf::Texture texture;
+    BOOST_CHECK(texture.getSize() == ::sf::Vector2<unsigned int>{});
+    BOOST_CHECK(1);
+  }
 }
 
-BOOST_AUTO_TEST_CASE(TestSFML)
-{
+BOOST_AUTO_TEST_CASE(TestRender) {
   {
-    //::sf::Texture texture;
-    //BOOST_CHECK(texture.getSize() == ::sf::Vector2<unsigned int> {});
-    BOOST_CHECK(1);
+    state::State state;
+    sf::RenderWindow window(sf::VideoMode(state.getBoard().getNCol() * 32 + 256,
+                                          state.getBoard().getNRow() * 32 + 32,
+                                          32),
+                            "SLAY - RENDER");
+    StateLayer sl(state, window);
+
+    BOOST_CHECK_EQUAL(sl.getTileHeight(), 84);
   }
 }
 
