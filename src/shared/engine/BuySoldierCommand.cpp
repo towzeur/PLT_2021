57e1980f0663d1d9df93a9ab540806d8ceaf0acc
Kaan <uygunkn@gmail.com>
2020-11-20 14:21:39 +0100

    buySoldier

--- src/shared/engine/BuySoldierCommand.cpp
+++ src/shared/engine/BuySoldierCommand.cpp
@@ -1,9 +1,24 @@
 #include "BuySoldierCommand.h"
+#include "Engine.h"
+#include "State.h"
+#include "Soldier.h"
 #include <iostream>
 
 using namespace state;
 using namespace engine;
+using namespace std;
 
-BuySoldierCommand::BuySoldierCommand(){}
+BuySoldierCommand::BuySoldierCommand(Territory territory){
+    int savings = territory.getSavings();
+    int soldierPrice = Soldier.getPrice();
+
+    if (savings>=soldierPrice){
+        // able to buy a soldier
+        Soldier soldier = Soldier();
+    }
+    else{
+        std::cout << "Unable to purchase a soldier."
+    }
+}
 
 void BuySoldierCommand::execute (state::State& state){}
