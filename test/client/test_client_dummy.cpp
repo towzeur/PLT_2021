9ef64cd3bbf8cd810c6e960027dee33c62b26b31
Badisse <badisse.bouabdallahb@laposte.net>
2020-11-23 00:00:31 +0100

    fix test

--- test/client/test_client_dummy.cpp
+++ test/client/test_client_dummy.cpp
@@ -14,17 +14,4 @@ BOOST_AUTO_TEST_CASE(TestSFML) {
   }
 }
 
-BOOST_AUTO_TEST_CASE(TestRender) {
-  {
-    state::State state;
-    sf::RenderWindow window(sf::VideoMode(state.getBoard().getNCol() * 32 + 256,
-                                          state.getBoard().getNRow() * 32 + 32,
-                                          32),
-                            "SLAY - RENDER");
-    StateLayer sl(state, window);
-
-    BOOST_CHECK_EQUAL(sl.getTileHeight(), 84);
-  }
-}
-
 /* vim: set sw=2 sts=2 et : */
