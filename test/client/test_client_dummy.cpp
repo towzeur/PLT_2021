7dac5422b7651a11f47a394c82c6c1ff450a862a
Badisse <badisse.bouabdallahb@laposte.net>
2020-11-23 09:36:38 +0100

    update test client

--- test/client/test_client_dummy.cpp
+++ test/client/test_client_dummy.cpp
@@ -7,8 +7,10 @@ BOOST_AUTO_TEST_CASE(TestSFML) {
   {
     ::sf::Texture texture;
     BOOST_CHECK(texture.getSize() == ::sf::Vector2<unsigned int>{});
-    BOOST_CHECK(1);
   }
 }
 
+BOOST_AUTO_TEST_CASE(TestClient) {
+  { BOOST_CHECK(1); }
+}
 /* vim: set sw=2 sts=2 et : */
