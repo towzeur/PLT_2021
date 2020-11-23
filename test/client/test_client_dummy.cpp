785093130382bfb3b1b0243d0c3400153b8c02a4
Badisse <badisse.bouabdallahb@laposte.net>
2020-11-23 09:02:28 +0100

    add test sfml

--- test/client/test_client_dummy.cpp
+++ test/client/test_client_dummy.cpp
@@ -1,13 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include <boost/test/unit_test.hpp>
 
-
 BOOST_AUTO_TEST_CASE(TestStaticAssert) { BOOST_CHECK(1); }
 
 BOOST_AUTO_TEST_CASE(TestSFML) {
   {
-    //::sf::Texture texture;
-    // BOOST_CHECK(texture.getSize() == ::sf::Vector2<unsigned int> {});
+    ::sf::Texture texture;
+    BOOST_CHECK(texture.getSize() == ::sf::Vector2<unsigned int>{});
     BOOST_CHECK(1);
   }
 }
