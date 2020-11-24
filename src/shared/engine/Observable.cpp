fe9637dba4843bf318b401c3252f6850e40b0575
hicham <hicham.sdiri1997@gmail.com>
2020-11-24 20:18:52 +0100

    Observable::notifyUpdating

--- src/shared/engine/Observable.cpp
+++ src/shared/engine/Observable.cpp
@@ -5,3 +5,9 @@
 using namespace engine;
 
 void Observable::registerObserver (Observer* obs){ observers.push_back(obs); }
+
+void Observable::notifyUpdating (){
+    for(auto Observer : observers){
+        Observer->engineUpdating();
+    }
+}
