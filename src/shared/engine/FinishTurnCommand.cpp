4349aeea889a1b542a0aaf4c9884dcdba7a24805
hicham <hicham.sdiri1997@gmail.com>
2020-11-25 10:04:54 +0100

    FinishTurnCommand::serialize

--- src/shared/engine/FinishTurnCommand.cpp
+++ src/shared/engine/FinishTurnCommand.cpp
@@ -8,3 +8,9 @@ using namespace std;
 
 
 FinishTurnCommand::FinishTurnCommand (){ Command::commandTypeId=FINISH_TURN; }
+
+Json::Value FinishTurnCommand::serialize (){
+    Json::Value newCommand;
+    return newCommand["commandTypeId"]= commandTypeId;
+}
+
