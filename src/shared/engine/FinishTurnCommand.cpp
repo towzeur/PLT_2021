9c7feccd6ff15241406015fb19d3e3229ce94d17
Badisse <badisse.bouabdallahb@laposte.net>
2020-11-27 22:23:32 +0100

    add reset PA and reset selected

--- src/shared/engine/FinishTurnCommand.cpp
+++ src/shared/engine/FinishTurnCommand.cpp
@@ -8,6 +8,27 @@ using namespace engine;
 FinishTurnCommand::FinishTurnCommand() { this->commandTypeId = FINISH_TURN; }
 
 void FinishTurnCommand::execute(state::State &state) {
+
+  std::vector<std::unique_ptr<state::Cell>> &cells =
+      state.getBoard().getCells();
+  for (std::unique_ptr<state::Cell> &soldier : cells) {
+    if (soldier->getEntity().isSoldier()) {
+      state::Soldier &s = dynamic_cast<state::Soldier &>(soldier->getEntity());
+      // Reset PA of all soldiers at the end of the turn
+      if (s.getPA() == 0) {
+        s.setPA(0);
+      }
+    }
+  }
+
+  std::vector<std::unique_ptr<state::Territory>> &territories =
+      state.getTerritories();
+
+  for (std::unique_ptr<state::Territory> &territory : territories) {
+    // Reset selected
+    territory->setSelected(false);
+  }
+
   state.setTurn(state.getTurn() + 1);
 }
 
