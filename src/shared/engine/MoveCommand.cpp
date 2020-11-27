42e6848d6efd4b3335677cb9f47827228945a76e
Badisse <badisse.bouabdallahb@laposte.net>
2020-11-27 20:02:32 +0100

    Update

--- src/shared/engine/MoveCommand.cpp
+++ src/shared/engine/MoveCommand.cpp
@@ -17,22 +17,107 @@ void MoveCommand::execute(state::State &state) {
 
   if (cellTarget.isAccessible()) {
     for (std::unique_ptr<state::Cell> &soldier : cells) {
-
+      // Catch the cell of the soldier
       if (soldier->getEntity().getUid() == this->soldierTarget.getUid()) {
-        if (soldier->getEntity().getAttack() <
-            this->cellTarget.getEntity().getDefense()) {
-          std::cout << "Impossible action: soldier attack must be superior or "
-                       "equal to cell entity defense"
-                    << std::endl;
-          break;
-        } else {
-          soldier->setRow(this->cellTarget.getRow());
-          soldier->setCol(this->cellTarget.getCol());
-          break;
+        // Check if the move is on the same territory
+        if (this->cellTarget.getPlayerId() == soldier->getPlayerId()) {
+          // Check if he try to move a soldier on one of his own facility
+          if (this->cellTarget.getEntity().getEntityTypeId() ==
+              state::EntityTypeId::FACILITY) {
+            std::cout << "Impossible action: your can't move a soldier on "
+                         "one of your facilitys"
+                      << std::endl;
+            break;
+          }
+          // Check if he try to do a fusion
+          else if (this->cellTarget.getEntity().getEntityTypeId() ==
+                   state::EntityTypeId::SOLDIER) {
+            int newAttack = this->cellTarget.getEntity().getAttack() +
+                            soldier->getEntity().getAttack();
+            int newDefense = this->cellTarget.getEntity().getDefense() +
+                             soldier->getEntity().getDefense();
+            if (newAttack > 4 || newDefense > 4) {
+              std::cout << "Impossible action: the result of your fusion "
+                           "can't give a soldier with an attack or a defense "
+                           "superior at 4"
+                        << std::endl;
+              break;
+            }
+            // Fusion
+            else {
+              if (newAttack == 2 && newDefense == 2) {
+                state::Entity *spearman = new state::Soldier(
+                    state::EntityTypeId::SOLDIER,
+                    state::SoldierTypeId::SPEARMAN, newAttack, newDefense);
+                this->cellTarget.setEntity(*spearman);
+                state::Entity *empty = new state::Empty(
+                    state::EntityTypeId::EMPTY, state::EmptyTypeId::VOID);
+                soldier->setEntity(*empty);
+              } else if (newAttack == 3 && newDefense == 3) {
+                state::Entity *knight = new state::Soldier(
+                    state::EntityTypeId::SOLDIER, state::SoldierTypeId::KNIGHT,
+                    newAttack, newDefense);
+                this->cellTarget.setEntity(*knight);
+                state::Entity *empty = new state::Empty(
+                    state::EntityTypeId::EMPTY, state::EmptyTypeId::VOID);
+                soldier->setEntity(*empty);
+              } else if (newAttack == 4 && newDefense == 4) {
+                state::Entity *baron = new state::Soldier(
+                    state::EntityTypeId::SOLDIER, state::SoldierTypeId::BARON,
+                    newAttack, newDefense);
+                this->cellTarget.setEntity(*baron);
+                state::Entity *empty = new state::Empty(
+                    state::EntityTypeId::EMPTY, state::EmptyTypeId::VOID);
+                soldier->setEntity(*empty);
+              } else {
+                std::cout << "Error fusion" << std::endl;
+                break;
+              }
+            }
+          }
+          // Cut a tree on his own territory
+          else if (this->cellTarget.getEntity().getEntityTypeId() ==
+                   state::EntityTypeId::TREE) {
+
+            state::Soldier &s =
+                dynamic_cast<state::Soldier &>(soldier->getEntity());
+            s.setPA(0);
+            this->cellTarget.setEntity(s);
+            state::Entity *empty = new state::Empty(state::EntityTypeId::EMPTY,
+                                                    state::EmptyTypeId::VOID);
+            soldier->setEntity(*empty);
+          }
+        }
+
+        // Check if he try to conquere a new territory
+        else {
+          if (soldier->getEntity().getAttack() <
+              this->cellTarget.getEntity().getDefense()) {
+            std::cout
+                << "Impossible action: soldier attack must be superior or "
+                   "equal to cell entity defense"
+                << std::endl;
+            break;
+          }
+          // Conquere a cell
+          else if (this->cellTarget.getEntity().getEntityTypeId() ==
+                   state::EntityTypeId::EMPTY) {
+            state::Soldier &s =
+                dynamic_cast<state::Soldier &>(soldier->getEntity());
+            s.setPA(0);
+            this->cellTarget.setEntity(s);
+            this->cellTarget.setTerritoryId(soldier->getTerritoryId());
+            this->cellTarget.setPlayerId(soldier->getPlayerId());
+            state::Entity *empty = new state::Empty(state::EntityTypeId::EMPTY,
+                                                    state::EmptyTypeId::VOID);
+            soldier->setEntity(*empty);
+          }
         }
       }
     }
-  } else {
+  }
+
+  else {
     std::cout << "Impossible action: innaccessible cell" << std::endl;
   }
 }
