aad1a32259ff87df23d762c89d70b2f12b2f87f4
hicham <hicham.sdiri1997@gmail.com>
2020-12-01 09:17:04 +0100

    Observable::unregisterObserver

--- src/shared/state/Observable.cpp
+++ src/shared/state/Observable.cpp
@@ -8,8 +8,13 @@ Observable::~Observable() {
 
 void Observable::registerObserver(Observer* o) { observers.push_back(o); }
 
-void Observable::unregisterObserver(Observer* o) { observers.pop_back();}
-
+void Observable::unregisterObserver(Observer* o) { 
+    for(auto Observer : observers){
+        if(Observer == o){
+            observers.pop_back();
+        }
+    }
+}
 void Observable::unregisterAllObservers() {
     observers.clear();
 }
