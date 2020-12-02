297bb8f62e96ad71cb8eb479ba2cb485e1d56061
towzeur <towzeur@gmail.com>
2020-12-03 00:31:53 +0100

    added Color to trigger sfmlGrahics include

--- extern/dia2code/src/generate_code_cpp.c
+++ extern/dia2code/src/generate_code_cpp.c
@@ -745,7 +745,7 @@ void print_include_stdlib(struct stdlib_includes *si, char *name) {
     }
     if (!si->sfmlGraphics &&
         (strstr(name, "sf::RenderWindow") || strstr(name, "sf::VertexArray") ||
-         strstr(name, "sf::Texture"))) {
+         strstr(name, "sf::Texture") || strstr(name, "sf::Color"))) {
       print("#include <SFML/Graphics.hpp>\n");
       si->sfmlGraphics = 1;
     }
