7a602adbbe949427edb134a5c8ba880d19056f17
Badisse <badisse.bouabdallahb@laposte.net>
2020-12-03 22:16:58 +0100

    Add territories initialisation

--- src/shared/engine/Engine.cpp
+++ src/shared/engine/Engine.cpp
@@ -11,12 +11,13 @@ Engine::~Engine() {}
 
 void Engine::init() {
 
-  std::string texts[] = {"Badisse", "Nico", "Hicham", "Kaan"};
-  state::Player p;
-  for (const std::string &text : texts) {
-    p = state::Player();
-    p.setName(text);
-    this->currentState.addPlayer(&p);
+  std::string names[] = {"Badisse", "Nico", "Hicham", "Kaan"};
+  bool neighborCellExist = true;
+
+  for (const std::string &name : names) {
+    std::unique_ptr<state::Player> p;
+    p->setName(name); // Fatal error test
+    this->currentState.addPlayer(move(p));
   }
 
   try {
@@ -26,8 +27,38 @@ void Engine::init() {
     this->currentState.getBoard().load("res/map.txt");
   }
 
+  // Init Territories
+  // Creation
+  for (auto &c : move(this->currentState.getBoard().getCells())) {
+    if (c->getEntity().getEntityTypeId() == state::EntityTypeId::FACILITY) {
+      std::unique_ptr<state::Territory> newTerritory;
+      newTerritory->setCapitalCoords(c->getRow(), c->getCol());
+      this->currentState.getPlayers()[c->getPlayerId()]->addTerrotory(
+          move(newTerritory));
+      c->setTerritoryId(newTerritory->getUid());
+    }
+  }
+  // Adding cells which compose the territory
+  for (auto &p : this->currentState.getPlayers()) {
+    std::vector<std::unique_ptr<state::Cell>> allPlayerCells;
+    for (auto &c : move(this->currentState.getBoard().getCells())) {
+      if (c->getPlayerId() == p->getUid()) {
+        allPlayerCells.push_back(move(c));
+      }
+    }
+    while (neighborCellExist) {
+      for (auto &c : move(allPlayerCells)) {
+        for (auto &cp : move(p->getTerritories()[0]->getCells())) {
+          if (cp->getCol() == c->getCol() && cp->getRow() == c->getRow() - 1) {
+            p->getTerritories()[0]->addCell(move(c));
+          }
+        }
+      }
+    }
+  }
+
   this->currentState.setTurn(1);
-  this->currentState.getPlayers()[0].setStatus(state::PLAYING);
+  this->currentState.getPlayers()[0]->setStatus(state::PLAYING);
 }
 
 void Engine::setCurrentState(state::State currentState) {
