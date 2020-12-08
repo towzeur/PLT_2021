c23658f5e2353f78f2337c8e6f80f205070922a6
towzeur <towzeur@gmail.com>
2020-12-08 17:50:36 +0100

    starting territoryTooltips

---
+++ src/client/render/TerritoryTooltips.cpp
@@ -0,0 +1,11 @@
+#include "TerritoryTooltips.h"
+
+using namespace render;
+
+TerritoryTooltips::TerritoryTooltips(RenderConfig &conf) {}
+void TerritoryTooltips::initialize() {}
+
+void TerritoryTooltips::update() {}
+
+void TerritoryTooltips::draw(sf::RenderTarget &target,
+                             sf::RenderStates states) {}
