e2482436e68d9e8bdf8b6b62f2dfae64cb172565
towzeur <towzeur@gmail.com>
2021-01-19 00:00:29 +0100

    One Entity to rules them all

--- src/shared/state/Entity.cpp
+++ src/shared/state/Entity.cpp
@@ -1,4 +1,5 @@
 #include "Entity.h"
+#include <stdexcept>
 
 using namespace state;
 
@@ -6,65 +7,95 @@ int Entity::instanceCount = 0;
 
 Entity::Entity() { this->uid = instanceCount++; }
 
+Entity::Entity(EntitySubTypeId subTypeId) {
+  this->uid = instanceCount++;
+
+  this->attack = 0;
+  this->defense = 0;
+  this->income = 0;
+  this->actionPoint = 0;
+
+  switch (subTypeId) {
+  case EMPTY_VOID: {
+    this->entityTypeId = state::EntityTypeId::EMPTY;
+    this->entitySubTypeId = state::EntitySubTypeId::EMPTY_VOID;
+    break;
+  }
+  case TREE_PINE: {
+    this->entityTypeId = state::EntityTypeId::TREE;
+    this->entitySubTypeId = state::EntitySubTypeId::TREE_PINE;
+    break;
+  }
+  case TREE_PALM: {
+    this->entityTypeId = state::EntityTypeId::TREE;
+    this->entitySubTypeId = state::EntitySubTypeId::TREE_PALM;
+    break;
+  }
+  case FACILITY_CAPITAL: {
+    this->entityTypeId = state::EntityTypeId::FACILITY;
+    this->entitySubTypeId = state::EntitySubTypeId::FACILITY_CAPITAL;
+    break;
+  }
+  case FACILITY_CASTLE: {
+    this->entityTypeId = state::EntityTypeId::FACILITY;
+    this->entitySubTypeId = state::EntitySubTypeId::FACILITY_CASTLE;
+    break;
+  }
+  case FACILITY_GRAVESTONE: {
+    this->entityTypeId = state::EntityTypeId::FACILITY;
+    this->entitySubTypeId = state::EntitySubTypeId::FACILITY_GRAVESTONE;
+    break;
+  }
+  case SOLDIER_PEASANT: {
+    this->entityTypeId = state::EntityTypeId::SOLDIER;
+    this->entitySubTypeId = state::EntitySubTypeId::SOLDIER_PEASANT;
+    break;
+  }
+  case SOLDIER_SPEARMAN: {
+    this->entityTypeId = state::EntityTypeId::SOLDIER;
+    this->entitySubTypeId = state::EntitySubTypeId::SOLDIER_SPEARMAN;
+    break;
+  }
+  case SOLDIER_KNIGHT: {
+    this->entityTypeId = state::EntityTypeId::SOLDIER;
+    this->entitySubTypeId = state::EntitySubTypeId::SOLDIER_KNIGHT;
+    break;
+  }
+  case SOLDIER_BARON: {
+    this->entityTypeId = state::EntityTypeId::SOLDIER;
+    this->entitySubTypeId = state::EntitySubTypeId::SOLDIER_BARON;
+    break;
+  }
+  default:
+    throw std::runtime_error("Unkown entity enum value");
+    break;
+  }
+}
+
 Entity::~Entity() {}
-
-bool const Entity::isEmpty() {
-  if (this->entityTypeId == EMPTY) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-bool const Entity::isTree() {
-  if (this->entityTypeId == TREE) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-bool const Entity::isFacility() {
-  if (this->entityTypeId == FACILITY) {
-    return true;
-  } else {
-    return false;
-  }
-}
-
-bool const Entity::isSoldier() {
-  if (this->entityTypeId == SOLDIER) {
-    return true;
-  } else {
-    return false;
-  }
-}
 
 int Entity::getUid() { return this->uid; }
 
-int Entity::getAttack() const { return this->attack; }
-
-void Entity::setAttack(int attack) { this->attack = attack; }
-
-int Entity::getDefense() const { return this->defense; }
-
-void Entity::setDefense(int defense) { this->defense = defense; }
-
-int Entity::getIncome() const { return this->income; }
-
-void Entity::setIncome(int income) { this->income = income; }
-
-int Entity::getSubTypeId() { return subTypeId; }
-
-void Entity::setEntityTypeId(EntityTypeId entityTypeId) {
-  this->entityTypeId = entityTypeId;
+bool Entity::isEmpty() {
+  return this->entityTypeId == state::EntityTypeId::EMPTY;
 }
 
-EntityTypeId const Entity::getEntityTypeId() { return this->entityTypeId; }
+bool Entity::isTree() {
+  return this->entityTypeId == state::EntityTypeId::TREE;
+}
 
-void Entity::setPA (int PA){ this->PA = PA;}
-int Entity::getPA (){return this->PA; }
+bool Entity::isFacility() {
+  return this->entityTypeId == state::EntityTypeId::FACILITY;
+}
 
-bool const Entity::equals(const Entity & element) {}
+bool Entity::isSoldier() {
+  return this->entityTypeId == state::EntityTypeId::SOLDIER;
+}
 
-Entity *const Entity::clone() {}
+EntityTypeId Entity::getEntityTypeId() { return this->entityTypeId; }
+
+EntitySubTypeId Entity::getEntitySubTypeId() { return this->entitySubTypeId; }
+
+void Entity::setActionPoint(int ap) { this->actionPoint = ap; }
+
+int Entity::getActionPoint() { return this->actionPoint; }
