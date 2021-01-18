f9372fd5e202fac97675e3a8e5a1bcc6e000dc92
towzeur <towzeur@gmail.com>
2021-01-18 23:57:46 +0100

    removed old stuff : legacy file path resolving

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -104,15 +104,7 @@ int main(int argc, char *argv[]) {
       // -----------------------------------------------------------------------
       //                               RENDER 2
       // -----------------------------------------------------------------------
-
-      char pBuf[256];
-      size_t len = sizeof(pBuf);
-      size_t bytes = readlink("/proc/self/exe", pBuf, len);
-      if (bytes >= 0 && bytes < len)
-        pBuf[bytes] = '\0';
-      std::cout << pBuf << std::endl;
-
-      client::Client clt = client::Client(); // = client::Client();
+      client::Client clt = client::Client();
       clt.run();
 
     } else if (arg1 == "engine") {
